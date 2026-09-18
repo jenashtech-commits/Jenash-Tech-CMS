bf3a106593020fb95f865338685481386ee5310c
Jenash e-commerce <jenashtech@gmail.com>
2026-09-18 16:26:26 +0000

    Update config.js

--- config.js
+++ config.js
@@ -9,6 +9,6 @@
 // After editing, save this file and reload index.html (or redeploy on Vercel).
 
 window.JENASH_CONFIG = {
-  SUPABASE_URL: "https://xxyvmrfzhqixkjjtmqsf.supabase.co/rest/v1/",
+  SUPABASE_URL: "https://xxyvmrfzhqixkjjtmqsf.supabase.co
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh4eXZtcmZ6aHFpeGtqanRtcXNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk3MDYyNjAsImV4cCI6MjEwNTI4MjI2MH0.0NTAm-x7blPMWjF3ZD4881P9mBFR7EZlrswvAn99q1A",
 };
